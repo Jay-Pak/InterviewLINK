462286da57b37a91a7293b63ed9a51b1865288b6
sincci1994 <sincci1994@gmail.com>
2022-12-27 22:56:04 +0900

    add 14.myinformation

--- lib/main.dart
+++ lib/main.dart
@@ -39,92 +39,148 @@ class _MyAppState extends State<MyApp> {
           ),
         ),
       ),
-      home: FAQ(),
+      home: myinformation(),
     );
   }
 }
 
-class FAQ extends StatelessWidget {
-  const FAQ({Key? key}) : super(key: key);
+class myinformation extends StatefulWidget {
+  const myinformation({Key? key}) : super(key: key);
+
+  @override
+  State<myinformation> createState() => _myinformation();
+}
+
+class _myinformation extends State<myinformation> {
+  int _current_index = 0;
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
+        title: const Text("내 정보"),
         leading: IconButton(
           icon: const Icon(Icons.arrow_back_ios_new),
           onPressed: () {},
         ),
-        title: const Text("자주 물어보는 질문"),
       ),
-      body: Container(
-        padding: EdgeInsets.only(top: 20),
-        child: ListView.builder(
-          itemCount: data.length,
-          itemBuilder: (BuildContext context, int index) =>
-              EntryItem(data[index]),
-        ),
+      body: Column(
+        children: [
+          Container(
+            child: Column(
+              children: [
+                Padding(
+                  padding: const EdgeInsets.only(left: 20, top: 20),
+                  child: Align(
+                    alignment: Alignment.bottomLeft,
+                    child: CircleAvatar(
+                      radius: 50,
+                    ),
+                  ),
+                ),
+                ListTile(
+                  title: Transform.translate(
+                    offset: Offset(10, 15),
+                    child: Text(
+                      '테라이님',
+                      style:
+                      TextStyle(fontSize: 24, fontWeight: FontWeight.bold),
+                    ),
+                  ),
+                  trailing: Transform.translate(
+                    offset: Offset(0, 15),
+                    child: IconButton(
+                      onPressed: () {},
+                      icon: Icon(Icons.navigate_next),
+                    ),
+                  ),
+                ),
+              ],
+            ),
+            decoration: BoxDecoration(
+                border: Border(
+                  bottom: BorderSide(width: 1.0, color: Colors.grey),
+                )),
+            height: 200,
+          ),
+          Expanded(
+            flex: 1,
+            child: ListView(
+              scrollDirection: Axis.vertical,
+              children: <Widget>[
+                ListTile(
+                  leading: Icon(Icons.notifications),
+                  trailing: IconButton(
+                    onPressed: () {},
+                    icon: Icon(Icons.navigate_next),
+                  ),
+                  title: Text('공지사항'),
+                  /*Transform.translate(
+                      offset: Offset(-20, -2), child: Text('문의하기')),*/
+                ),
+                ListTile(
+                  leading: Icon(Icons.question_answer),
+                  trailing: IconButton(
+                    onPressed: () {},
+                    icon: Icon(Icons.navigate_next),
+                  ),
+                  title: Text('자주 물어보는 질문'),
+                  /*Transform.translate(
+                      offset: Offset(-20, -2), child: Text('문의하기')),*/
+                ),
+                ListTile(
+                  leading: Icon(Icons.favorite),
+                  trailing: IconButton(
+                    onPressed: () {},
+                    icon: Icon(Icons.navigate_next),
+                  ),
+                  title: Text('리뷰 남기'),
+                  /*Transform.translate(
+                      offset: Offset(-20, -2), child: Text('문의하기')),*/
+                ),
+                ListTile(
+                  leading: Icon(Icons.content_copy),
+                  trailing: IconButton(
+                    onPressed: () {},
+                    icon: Icon(Icons.navigate_next),
+                  ),
+                  title: Text('서비스 이용약관'),
+                  /*Transform.translate(
+                      offset: Offset(-20, -2), child: Text('문의하기')),*/
+                ),
+                ListTile(
+                  leading: Icon(Icons.content_copy),
+                  trailing: IconButton(
+                    onPressed: () {},
+                    icon: Icon(Icons.navigate_next),
+                  ),
+                  title: Text('개인정보처리방침'),
+                  /*Transform.translate(
+                      offset: Offset(-20, -2), child: Text('문의하기')),*/
+                ),
+                ListTile(
+                  leading: Icon(Icons.contact_support),
+                  trailing: IconButton(
+                    onPressed: () {},
+                    icon: Icon(Icons.navigate_next),
+                  ),
+                  title: Text('문의하기'),
+                  /*Transform.translate(
+                      offset: Offset(-20, -2), child: Text('문의하기')),*/
+                ),
+                ListTile(
+                  leading: Icon(Icons.logout),
+                  trailing: IconButton(
+                    onPressed: () {},
+                    icon: Icon(Icons.navigate_next),
+                  ),
+                  title: Text('로그아웃'),
+                ),
+              ],
+            ),
+          ),
+        ],
       ),
     );
-  }
-}
-
-// One entry in the multilevel list displayed by this app.
-class Entry {
-  Entry(this.title, [this.content = const <Entry>[]]);
-
-  final String title;
-  final List<Entry> content;
-}
-
-// The entire multilevel list displayed by this app.
-final List<Entry> data = <Entry>[
-  Entry(
-    '왜 이딴걸 하나요?',
-    <Entry>[
-      Entry(
-          "돈 벌라고요 돈 벌라고요 돈 벌라고요 돈 벌라고요 돈 벌라고요 돈 벌라고요 돈 벌라고요 돈 벌라고요 돈 벌라고요 돈 벌라고요 돈 벌라고요 돈 벌라고요 돈 벌라고요"
-          "22®돈 벌라고요 돈 벌라고요 돈 벌라고요 돈 벌라고요 돈 벌라고요 돈 벌라고요 돈 벌라고요 돈 벌라고요 돈 벌라고요 돈 벌라고요 돈 벌라고요 돈 벌라고요 돈 벌라고요 돈 벌라고요 "),
-    ],
-  ),
-  Entry(
-    '괴롭나요',
-    <Entry>[
-      Entry(
-          '정신나갈것같애정신나갈것같애 정신나갈것같애정신나갈것같애정신나갈것같애 정신나갈것같애정신나갈것같애정신나갈것같애 정신나갈것같애정신나갈것같애정신나갈것같애 정신나갈것같애'
-          '정신나갈것같애정신나갈것같애정신나갈것같애정신나갈것같애정신나갈것같애정신나갈것같애정신나갈것같애정신나갈것같애정신나갈것같애정신나갈것같애정신나갈것같애정신나갈것같애정신나갈것같애'
-          '정신나갈것같애정신나갈것같애정신나갈것같애정신나갈것같애정신나갈것같애정신나갈것같애정신나갈것같애'),
-    ],
-  ),
-];
-
-// Displays one Entry. If the entry has content then it's displayed
-// with an ExpansionTile.
-class EntryItem extends StatelessWidget {
-  const EntryItem(this.entry);
-
-  final Entry entry;
-
-  Widget _buildTiles(Entry root) {
-    if (root.content.isEmpty)
-      return ListTile(
-          title: Text(
-        root.title,
-        style: TextStyle(fontSize: 16),
-      ));
-    return ExpansionTile(
-      key: PageStorageKey<Entry>(root),
-      title: Text(
-        root.title,
-        style: TextStyle(fontSize: 16),
-      ),
-      leading: Icon(Icons.question_answer),
-      children: root.content.map(_buildTiles).toList(),
-    );
-  }
-
-  @override
-  Widget build(BuildContext context) {
-    return _buildTiles(entry);
   }
 }
